744ca44ae96dad9a36d5613d96bfa71228b4a3e5
nathanwchan <nchan87@gmail.com>
2014-02-23 08:16:31 -0800

    Assignment 7 submission

--- assignments/session07/mysite/myblog/admin.py
+++ assignments/session07/mysite/myblog/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ('name', 'description')
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
